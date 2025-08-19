60ed9e7949249cb893e0bccc4dd2a08e07fefdb9
Hitesh04codebot <hiteshkandari@gmail.com>
2025-08-19 23:45:19 +0530

    created responsive navbar

--- src/components/UI/Header.jsx
+++ src/components/UI/Header.jsx
@@ -1,9 +1,97 @@
-import React from 'react'
+import React, { use } from "react";
+import { Link } from "react-router-dom";
+import { FiAlignJustify } from "react-icons/fi";
+import { FiX } from "react-icons/fi";
+import { useState } from "react";
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <h1>Header</h1>
-  )
+    <>
+      <div className="flex flex-row w-auto h-15 bg-zinc-800 p-10 items-center justify-around max-md:flex max-md:justify-around">
+        <div className="flex items-center justify-end cursor-pointer">
+          <Link to="/">
+            <h1 className="text-4xl font-bold cursor-pointer text-white p-4">
+              AtlasPedia
+            </h1>
+          </Link>
+        </div>
+        <nav className="max-md:hidden flex items-center justify-center">
+          <ul className="flex flex-row gap-x-10 text-white">
+            <Link to="/" onClick={() => setHamburger(false)}>
+              <li className="transition transform hover:scale-105 hover:text-zinc-300">
+                Home
+              </li>
+            </Link>
+            <Link to="/about">
+              <li className="transition transform hover:scale-105 hover:text-zinc-300">
+                About
+              </li>
+            </Link>
+            <Link to="/country">
+              <li className="transition transform hover:scale-105 hover:text-zinc-300">
+                Country
+              </li>
+            </Link>
+            <Link to="/contact">
+              <li className="transition transform hover:scale-105 hover:text-zinc-300">
+                Contact
+              </li>
+            </Link>
+          </ul>
+        </nav>
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          className="flex items-center justify-end text-white cursor-pointer md:hidden text-3xl font-bold"
+        >
+          {isOpen ? <FiX /> : <FiAlignJustify />}
+        </button>
+      </div>
+
+      {isOpen ? (
+        <nav className="md:hidden bg-zinc-800 text-white w-full h-auto absolute top-15 left-0">
+          <ul className="flex flex-col items-center justify-center gap-y-3 p-3">
+            <li>
+              <Link
+                to="/"
+                onClick={() => setHamburger(false)}
+                className="text-xl"
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/about"
+                onClick={() => setHamburger(false)}
+                className="text-xl"
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/country"
+                onClick={() => setHamburger(false)}
+                className="text-xl"
+              >
+                Country
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/contact"
+                onClick={() => setHamburger(false)}
+                className="text-xl"
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      ) : null}
+    </>
+  );
 }
 
-export default Header
+export default Header;
--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,14 +1,27 @@
-import notfoundimg from '../assets/images/notfoundimg.png';
+import notfoundimg from "../assets/images/notfoundimg.png";
+import { Link } from "react-router-dom";
 function ErrorPage() {
   return (
     <>
-    <div className="flex items-center justify-center h-screen">
-      <div className='flex flex-col items-center justify-center w-[650px] m-3 p-3 gap-3'>
-        <img src={notfoundimg} alt="Error"/>
-        <p className="text-4xl font-bold text-center max-sm:text-3xl">Oops! Page Not Found.</p>
-        <p className='text-2xl font-normal text-center max-sm:text-xl'>You must have picked the wrong door because I haven't been able to lay my eyes on the page you are searching for. </p>
+      <div className="flex items-center justify-center h-screen bg-zinc-900">
+        <div className="flex flex-col items-center justify-center w-[650px] m-3 p-3 gap-3">
+          <img src={notfoundimg} alt="Error" />
+          <p className="text-4xl font-bold text-center max-sm:text-3xl text-white">
+            Oops! Page Not Found.
+          </p>
+          <p className="text-2xl font-normal text-center max-sm:text-xl text-white">
+            You must have picked the wrong door because I haven't been able to
+            lay my eyes on the page you are searching for.{" "}
+          </p>
+          <Link
+            to="/"
+            className="block px-8 py-3 bg-blue-600 text-white font-bold rounded-md 
+             transition transform hover:scale-110 hover:bg-blue-700 duration-300"
+          >
+            Go Back Home
+          </Link>
+        </div>
       </div>
-    </div>  
     </>
   );
 }
